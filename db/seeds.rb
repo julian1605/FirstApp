977e4dcef242c59f9bf9641d1dd46ffbaa765d0d
Julian <julian.edelmann@yahoo.de>
2016-11-28 00:31:07 +0100

    4.2.2 added color and price field to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 		
-		
+		Product.create(:name => "seed-generated product2", :description => "I also created this product without using the HTML form!")
