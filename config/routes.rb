7088d544a034697fc2877c9993a8d5257f0b2085
Julian <julian.edelmann@yahoo.de>
2016-12-22 14:45:44 +0100

    5.1 Changing routes and redirect

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-	root 'static_pages#landing_page'
+	root 'static_pages#index'
 
 	resources :orders, only: [:index, :show, :create, :destroy]
 
