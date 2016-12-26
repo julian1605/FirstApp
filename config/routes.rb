470ae8a6f63e04d3f8d88ff7fd4b9f4e0f530d87
Julian <julian.edelmann@yahoo.de>
2016-12-26 21:44:27 +0100

    5.3.1 Sendgrit added

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
+  post 'static_pages/thank_you'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 	root 'static_pages#index'
