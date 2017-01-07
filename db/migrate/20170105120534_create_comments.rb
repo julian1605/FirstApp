b2c796d95d5fdcc01414e133088fd7334ee342ec
Julian <julian.edelmann@yahoo.de>
2017-01-07 14:29:59 +0100

    5.7 fixed migration problems

--- db/migrate/20170105120534_create_comments.rb
+++ db/migrate/20170105120534_create_comments.rb
@@ -1,5 +1,6 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
+  	drop_table :comments
     create_table :comments do |t|
       t.references :user, foreign_key: true
       t.text :body
