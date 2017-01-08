88430953f9b52401c2a155a87f16f1ce1c067d97
Julian <julian.edelmann@yahoo.de>
2017-01-08 14:31:56 +0100

    5.10 added admin role and permissions for admins and users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,13 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
     can :read, Order, :user_id => user.id
+    can :create, :Comment
+    can :read, :Comment
+    can :read, :Product
+    
+    if user.admin?
+      can :manage, :all
+    end
   end
 
 end
