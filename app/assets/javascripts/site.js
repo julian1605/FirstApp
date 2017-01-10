4c6b53258dd86fd34b7292b1ccf54afe9023e029
Julian <julian.edelmann@yahoo.de>
2017-01-10 15:41:09 +0100

    5.X.3 deleting document ready function

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -16,13 +16,7 @@ $(document).on('turbolinks:load', function(){
 // });
 
 
-$( document ).ready(function() {
-	$('.alert, .alert-info, .alert-warning').delay(3000)fadeOut(2000);
-	jQuery(window).load(function () {
-		$(".how_it_works_col_1, .how_it_works_col_2, .how_it_works_col_3, .how_it_works_col_4").fadeOut(2000);
-    $(".how_it_works_col_1, .how_it_works_col_2, .how_it_works_col_3, .how_it_works_col_4").fadeIn(4500);
-	});
-});
+
 
 
 $(".alert, .alert-info, .alert-warning" ).delay(3000)fadeOut(2000);
