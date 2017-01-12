c14ca24048bed5715fe54e042ea6040e91aef753
Julian <julian.edelmann@yahoo.de>
2017-01-12 20:45:54 +0100

    5.X.15 show images on Heroku (like described on StackOverflow

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+#Added to show raty images on Heroku
+Rails.application.config.assets.precompile += %w( ratyrate.js )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
