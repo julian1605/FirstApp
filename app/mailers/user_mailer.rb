d03addd2baad46417187fca1893f880fef082f1a
Julian <julian.edelmann@yahoo.de>
2017-01-18 14:13:18 +0100

    5.11.1 adding email on singup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,5 +8,11 @@ class UserMailer < ApplicationMailer
         :to => 'julian.edelmann@yahoo.de',
         :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  @appname = "breakfastDelivery"
+  mail( :to => user.email,
+        :subject => "Welcome to #{@appname}!")
+end
   
 end
