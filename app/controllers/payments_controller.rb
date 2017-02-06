d6c883f27b8d40576b996c7031ee99efb93ee435
Julian <julian.edelmann@yahoo.de>
2017-02-06 18:02:13 +0100

    6.6 fixing payments_controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -15,7 +15,7 @@ class PaymentsController < ApplicationController
 	   if charge.paid
 	   	Order.create(
 	   		:product_id => @product.id,
-	   		:user_id => @user_id,
+	   		:user_id => @user.id,
 	   		:total => @product.price
 	   		)
 	   	UserMailer.product_paid(@user, @product).deliver_now
