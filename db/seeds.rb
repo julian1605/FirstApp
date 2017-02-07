cc722e59cda890b221487b11bb5b357f1ccd5453
Julian <julian.edelmann@yahoo.de>
2017-02-07 09:14:31 +0100

    removing elevateZoom from application.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 		
-		Product.create(:name => "seed-generated product2", :description => "I also created this product without using the HTML form!")
-		Product.create(:name => 'Seed Product', :description => 'This is a product descitption for a product that is introduced in the seed.rb file', :image_url => '/assets/football.jpg')
+		Product.create(:name => "seed-generated product2", :description => "I also created this product without using the HTML form!", :price => 2.23)
+		Product.create(:name => 'Seed Product', :description => 'This is a product descitption for a product that is introduced in the seed.rb file', :image_url => '/assets/football.jpg' :price => 43.22)
