ee1c43719a2aa15892fdf04ba696ae652b5d07e8
Julian <julian.edelmann@yahoo.de>
2017-02-08 11:09:24 +0100

    6.12.2 fixing application. rb

--- config/application.rb
+++ config/application.rb
@@ -16,13 +16,13 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-#redis
-config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
-
 module FirstApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #redis
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
   end
 end
